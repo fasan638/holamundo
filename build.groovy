332544a041e7a56a5b833c8cb3694d58f0e918cb
Fernando Sanchez <81205288+fasan638@users.noreply.github.com>
2023-10-23 21:09:11 -0300

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -43,7 +43,7 @@ pipeline{
         }
         stage('Upload to nexus'){
             agent {
-                label 'maven'
+                label 'docker'
             }
             steps{
                 script{
